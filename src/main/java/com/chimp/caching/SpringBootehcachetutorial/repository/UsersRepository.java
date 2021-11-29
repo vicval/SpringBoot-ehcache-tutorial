c5d1f6fc774c6250c9a6cf412e22859624c0b056
vicente <vicente.franca.valcarcel@gmail.com>
2021-11-29 17:28:04 -0300

    solve dependency problem

--- src/main/java/com/chimp/caching/SpringBootehcachetutorial/repository/UsersRepository.java
+++ src/main/java/com/chimp/caching/SpringBootehcachetutorial/repository/UsersRepository.java
@@ -3,7 +3,9 @@ package com.chimp.caching.SpringBootehcachetutorial.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.chimp.caching.SpringBootehcachetutorial.model.Users;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsersRepository extends JpaRepository<Users, Long> {
     Users findByName(String name);
 }
